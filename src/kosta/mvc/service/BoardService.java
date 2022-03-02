0eb94a06d2a7c07d407778a4925434541b615d27
ChanBum <rew9732@gmail.com>
2022-03-02 22:24:58 +0900

    BoardService,MainView수정하기

--- src/kosta/mvc/service/BoardService.java
+++ src/kosta/mvc/service/BoardService.java
@@ -5,4 +5,8 @@ public class BoardService {
     	
     }
     
+    public void test() {
+    	
+    }
+    
 }
--- src/kosta/mvc/view/MainView.java
+++ src/kosta/mvc/view/MainView.java
@@ -1,5 +1,5 @@
 package kosta.mvc.view;
 
 public class MainView {
-
+    public void aa() {}
 }
